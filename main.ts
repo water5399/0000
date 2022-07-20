bae96b4aae5f1d99f8aa45a00b348f46cf1d10ca
water5399 <107670475+water5399@users.noreply.github.com>
2022-07-20 14:42:35 +0800

    1011010101

--- main.ts
+++ main.ts
@@ -1,43 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("000000000000000000")
 })
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # . # .
-    . # # # .
-    . . . . .
-    `)
-basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . . . #
-    # . . . #
-    # # # # #
-    `)
-basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # . # #
-    # # # # #
-    # # # # #
-    `)
-basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    # # # # #
-    `)
+pins.analogWritePin(AnalogPin.P0, 1023)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
 })
